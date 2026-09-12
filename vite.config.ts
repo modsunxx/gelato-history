2903c82de6a8b3ff88d3811ceb89e3bd75f98192
modsunxx <modsunxx@gmail.com>
2026-09-12 09:07:46 +0700

    update: vite.config.ts for cancel to find react-native-fs

--- vite.config.ts
+++ vite.config.ts
@@ -18,5 +18,14 @@ export default defineConfig({
 			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 			adapter: adapter()
 		})
-	]
+	],
+	build: {
+		rollupOptions: {
+			external: ['react-native-fs']
+		},
+		// อิงตาม Error Log ของ Vite ตัวใหม่ที่ใช้ Rolldown
+		rolldownOptions: {
+			external: ['react-native-fs']
+		}
+	}
 });
